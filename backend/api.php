4987a0eec5e5c103535e7e12a057b0d118373e93
furkanGit <furkancaliskan9801@gmail.com>
2023-01-13 13:10:41 +0100

    Created api_create_new_task and api.php

---
+++ backend/api.php
@@ -0,0 +1,9 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-Type: application/json');
+
+$jsonTodoList = file_get_contents("todo.json");
+echo $jsonTodoList;
